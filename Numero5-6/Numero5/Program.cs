9b77dcef48795bc0bf2d6443bbe743ad303b8b60
PierreCarrione <50272580+PierreCarrione@users.noreply.github.com>
2022-11-15 08:58:56 -0300

    Add files via upload

---
+++ Numero5-6/Numero5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+
+public class WrongTime : Exception
+{
+    public WrongTime(String message)
+        : base(message) { }
+}
+public class Intervalo
+{
+    private readonly DateTime inicial;
+    private readonly DateTime final;
+    TimeSpan Duracao;
+    public Intervalo(DateTime inicial, DateTime final)
+    {
+        if (inicial > final)
+        {
+            throw new WrongTime("Initial time can't be greater than final time");
+        }
+        else
+        {
+            this.inicial = inicial;
+            this.final = final;
+            Duracao = final - inicial;
+        }
+    }
+
+    public DateTime getIinicial()
+    {
+        return this.inicial;    
+    }
+
+    public DateTime getFinal()
+    {
+        return this.final;
+    }
+
+    public TimeSpan getDuracao()
+    {
+        return this.Duracao;
+    }
+    public Boolean TemIntersecao(DateTime inicialComp, DateTime finalComp)
+    {
+        if ((getIinicial() <= inicialComp && inicialComp <= getFinal() ) || inicialComp <= getIinicial() && finalComp > getIinicial())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean SaoIguais(DateTime inicialComp, DateTime finalComp)
+    {
+        if (getIinicial() == inicialComp && getFinal() == finalComp)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void Main(String[] args)
+    {
+        Intervalo a;
+
+        try
+        {
+            a = new Intervalo(new DateTime(2015, 12, 20), new DateTime(2018, 12, 31));
+            Console.WriteLine(a.getIinicial());
+            Console.WriteLine(a.getDuracao());
+            Console.WriteLine(a.TemIntersecao(new DateTime(2013, 12, 20), new DateTime(2014, 12, 31)));
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Numero5-6/Numero6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public class ListaIntervalo
+{
+    List<Intervalo> intervalos = new List<Intervalo>();
+    public void Add(DateTime inicial, DateTime final)
+    {
+        int flag = 0;
+
+        for (int i = 0; i < intervalos.Count; i++)
+        {
+            if (intervalos[i].TemIntersecao(inicial, final))
+            {
+                flag++;
+            }
+        }
+        if ( flag == 0)
+        {
+            intervalos.Add(new Intervalo(inicial,final));
+        }
+    }
+
+    public void Imprime()
+    {
+        Intervalo aux;
+        int y = 1;
+
+        for (int i = 0; i < intervalos.Count-1; i++)
+        {
+            for (y = 1; y < intervalos.Count;y++)
+            {
+                if (intervalos[i].getFinal() > intervalos[y].getIinicial())
+                {
+                    aux = intervalos[y];
+                    intervalos[y] = intervalos[i];
+                    intervalos[i] = aux;
+                }
+            }
+            y++;
+        }
+        for (int i = 0; i < intervalos.Count; i++)
+        {
+            Console.WriteLine("{0} {1}", intervalos[i].getIinicial(), intervalos[i].getFinal());
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        ListaIntervalo lista = new ListaIntervalo();
+        lista.Add(new DateTime(2015, 12, 20), new DateTime(2016, 12, 31));
+        lista.Add(new DateTime(2017, 12, 20), new DateTime(2018, 12, 31));
+        lista.Add(new DateTime(2012, 12, 20), new DateTime(2013, 12, 31));
+
+        lista.Imprime();
+    }
+}
